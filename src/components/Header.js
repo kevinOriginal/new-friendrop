89bc1f7267b3e9690e4edf106c4ef1777bec1345
GodofKim <kjh.original@gmail.com>
2016-09-18 02:44:51 +0900

    ADD : Header component

---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+
+const propTypes = {
+
+};
+
+const defaultProps = {
+  authenticated : false
+};
+
+class Header extends React.Component {
+
+  constructor(props) {
+      super(props);
+  }
+
+  renderBrand() {
+    let route;
+
+    if(this.props.authenticated)
+    { route = '/todaydrop'; }
+    else
+    { route = '/'; }
+
+    return <Link to={route} className="navbar-brand">Friendrop</Link>;
+  }
+  render() {
+    const loginedMenu = [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/profile">프로필</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/todaydrop">오늘의 드랍</Link>
+        </li>,
+        <li className="nav-item" key={3}>
+          <Link className="nav-link" to="/letter">쪽지</Link>
+        </li>,
+        <li className="nav-item" key={4}>
+          <Link className="nav-link" to="/contact">연락처</Link>
+        </li>,
+        <li className="nav-item" key={5}>
+          <Link className="nav-link" to="/logout">로그아웃</Link>
+        </li>
+    ];
+    const notLoginedMenu = [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/">로그인</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">가입하기</Link>
+        </li>
+      ];
+
+    return(
+      <nav className="navbar navbar-light">
+        {this.renderBrand()}
+        <ul className="nav navbar-nav">
+          { this.props.authenticated ? loginedMenu : notLoginedMenu }
+        </ul>
+      </nav>
+    );
+  }
+}
+
+Header.propTypes = propTypes;
+Header.defaultProps = defaultProps;
+
+export default Header;
