a726caf4fad30d2e6490300ddbff6d4fd61e5279
GodofKim <kjh.original@gmail.com>
2016-09-22 01:29:20 +0900

    UPDATE : add Header to App.js
    components폴더의 index에서 Header를 가져오기 떄문에 '../components' 이다.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Header } from '../components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         {this.props.children}
       </div>
     );
