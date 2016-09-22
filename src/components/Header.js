57f5317caa0d49660bc5472992d2ac1cbc3d2bc1
GodofKim <kjh.original@gmail.com>
2016-09-23 01:05:15 +0900

    Fix Link path ( / -> /login)
    로그인 버튼 누르면 홈페이지가 아니라 로그인 페이지로 가도록 수정하다

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,7 +45,7 @@ class Header extends React.Component {
     ];
     const notLoggedInMenu = [
         <li className="nav-item" key={1}>
-          <Link className="nav-link" to="/">로그인</Link>
+          <Link className="nav-link" to="/login">로그인</Link>
         </li>,
         <li className="nav-item" key={2}>
           <Link className="nav-link" to="/signup">가입하기</Link>
