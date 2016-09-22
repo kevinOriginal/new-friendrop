e12923a66412d48e231943ac03f63fb8a73dcbf5
GodofKim <kjh.original@gmail.com>
2016-09-23 01:12:24 +0900

    Modify : Add handleLogin to Login component
    액션 생성자인 loginRequest를 불러오고 handleLogin 함수에서 사용하였다. 서버에서 온 응답의 성공/ 실패에 따라
    toast 알람이 뜨도록 했다.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { Authentication } from 'components';
+import { Authentication } from '../components';
 import { connect } from 'react-redux';
-//import { loginRequest } from 'actions/authentication';
+import { loginRequest } from '../actions/authentication';
 import { browserHistory } from 'react-router';
 
 class Login extends React.Component {
@@ -13,7 +13,28 @@ class Login extends React.Component {
   }
 
   handleLogin(id, pw){
+    return this.props.loginRequest(id, pw).then(
+      () => {
+        if(this.props.status === "SUCCESS") {
+          let loginData = {
+            isLoggedIn: true,
+            email: id
+          };
 
+          document.cookie = 'key=' + btoa(JSON.stringify(loginData));
+          // 쿠키는 왜 만드는 거더라.
+
+          Materialize.toast('Welcome!', 2000);
+          browserHistory.push('/');
+          return true;
+        }
+        else {
+          let $toastContent = ('<span style="color: #FFB4BA">Incorrect username or password</span>');
+          Materialize.toast($toastContent, 2000);
+          return false;
+        }
+      }
+    );
   }
 
   render() {
@@ -28,6 +49,14 @@ class Login extends React.Component {
 const mapStateToProps = (state) => {
   return {
     status: state.authentication.login.status
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loginRequest: (id, pw) => {
+      return dispatch(loginRequest(id, pw));
+    }
   };
 };
 
